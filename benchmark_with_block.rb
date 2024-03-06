471918a92e2e97c714fbe782f74cc7280036334c
Rico Chow <rico.ck.chow@gmail.com>
2024-03-05 21:05:29 -0500

    Add solution for benchmark with block

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,10 +1,13 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield if block_given?
+  end_time = Time.now
+  end_time - start_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
-long_string = "apple"*100000000
+long_string = "apple" * 100000000
  
 running_time = benchmark { long_string.reverse }
  
